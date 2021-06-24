6d0b1fe527dc7e422049c3f0baad59cbccf48925
quyctd <quy.dc98@gmail.com>
2021-06-24 21:33:14 +0700

    Sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import Topbar from "./app/components/Topbar"
+import Sidebar from "./app/components/Sidebar"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="container">
+        <Sidebar />
+      </div>
     </div>
   )
 }
---
+++ src/app/components/Sidebar/index.tsx
@@ -0,0 +1,102 @@
+import React from "react"
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PermIdentity,
+  Storefront,
+  AttachMoney,
+  EqualizerRounded,
+  MarkunreadOutlined,
+  DynamicFeedRounded,
+  ChatBubbleOutlineRounded,
+  WorkOutlineRounded,
+  Report,
+} from "@material-ui/icons"
+import "./index.css"
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyle className="sidebarIcon" />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" />
+              Sales
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <PermIdentity className="sidebarIcon" />
+              Users
+            </li>
+            <li className="sidebarListItem">
+              <Storefront className="sidebarIcon" />
+              Products
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoney className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <EqualizerRounded className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MarkunreadOutlined className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeedRounded className="sidebarIcon" />
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutlineRounded className="sidebarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <WorkOutlineRounded className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <Report className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
