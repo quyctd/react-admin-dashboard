622dff3abfc8005bc1e6e184f965ac1cce549bfe
quyctd <quy.dc98@gmail.com>
2021-06-26 21:35:49 +0700

    Add link home

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import {
   LineStyle,
   Timeline,
@@ -23,10 +22,12 @@ function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem active">
-              <LineStyle className="sidebarIcon" />
-              Home
-            </li>
+            <Link to="/" className="link">
+              <li className="sidebarListItem">
+                <LineStyle className="sidebarIcon" />
+                Home
+              </li>
+            </Link>
             <li className="sidebarListItem">
               <Timeline className="sidebarIcon" />
               Analytics
